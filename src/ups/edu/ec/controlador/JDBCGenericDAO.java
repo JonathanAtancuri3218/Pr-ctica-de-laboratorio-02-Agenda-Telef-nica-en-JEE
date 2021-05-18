19de5e710560a767acb850f51e34897d243e00e8
JonathanAtancuri3218 <JOTA5613jhon>
2021-05-18 02:14:57 -0500

    cambios actualizacion  README

--- src/ups/edu/ec/controlador/JDBCGenericDAO.java
+++
@@ -1,7 +0,0 @@
-package ups.edu.ec.controlador;
-
-public abstract class JDBCGenericDAO<T, ID> implements GenericDAO<T, ID>{
-
-	protected ContextJDBC conexionuno=ContextJDBC.getJDBC1();
-	protected ContextJDBC conexionDos = ContextJDBC.getJDBC2();
-}
